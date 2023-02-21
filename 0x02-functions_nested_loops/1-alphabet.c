789071a6128c0c49029a122a728a8b0267f1b2f7
akinmuizz <akinmuizz@gmail.com>
2023-02-21 11:05:43 -0800

    s

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check code
+ *
+ * Return: always 0
+ */
+
+void print_alphabet(void);
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+
+}
