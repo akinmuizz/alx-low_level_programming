09bf38f67377ed2e3d485e6b59dfe0c6ff5bbe96
akinmuizz <akinmuizz@gmail.com>
2023-02-21 14:39:33 -0800

    s

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *
- * Return: Always 0 (success)
+ * print_sign - prints the signs of numbers
+ * @n: The number that will be checked
+ * Return: 0 always
  */
-int main(void)
+
+int print_sign(int n)
+
 {
 	if (n > 0)
 	{
 		_putchar(43);
 		return (1);
 	}
-	else if (n > 0)
+	else if (n < 0)
 	{
 		_putchar(45);
 		return (-1);
