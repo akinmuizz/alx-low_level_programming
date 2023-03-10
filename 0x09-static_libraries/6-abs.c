617ed25cc99cbd1f3203b9892e281d2bfb58afeb
akinmuizz <akinmuizz@gmail.com>
2023-03-10 05:04:41 -0800

    d

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _abs - done for number 6
+ * @n: an integer
+ * Return: a success
+ */
+
+int _abs(int n)
+{
+	return abs(n);
+}
