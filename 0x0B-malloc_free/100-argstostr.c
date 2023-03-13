18fe6153c97c55a49b0bdde45ab08cb156982756
akinmuizz <akinmuizz@gmail.com>
2023-03-13 05:53:39 -0700

    s

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,11 +10,7 @@
 char *argstorstr(int ac, char **av)
 {
 	char *str;
-	int count = 0;
-
-	a = 0;
-	b = 0;
-	c = 0;
+	int count = 0, a = 0, b = 0, c = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
