e43024fbc568e1b1782d932607a147460a57f9a1
akinmuizz <akinmuizz@gmail.com>
2023-04-05 04:09:22 -0700

    d

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * pop_listint -Deletes head notes, amazing
+ * @head: the pointer that points to the head node
+ *
+ * Return: Always 0 if linked list is empty
+ */
+
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *tmp;
+
+	if (*head == NULL)
+	{
+		return (0);
+	}
+
+	n = (*head)->n;
+	tmp = (*head)->next;
+	free(*head);
+	tmp = *head;
+
+	return (n);
+}
