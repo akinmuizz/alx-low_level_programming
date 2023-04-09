ef2067f97d70e756831f749c07b82a511ceb2dbf
akinmuizz <akinmuizz@gmail.com>
2023-04-09 03:43:00 -0700

    s

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,9 +2,9 @@
 #define MAIN_H
 
 #include <sys/types.h>
-#include <sys/stats.h>
+#include <sys/stat.h>
 #include <fcntl.h>
-#include<unistd.h>
+#include <unistd.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
